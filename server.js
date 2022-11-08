a8abd3b9efc1f80f756c8317ad8609011973556e
Sujon Hossain <sujon.hossain758@gmail.com>
2022-11-08 16:01:08 +0600

    server ready

--- server.js
+++ server.js
@@ -1,2 +1,23 @@
-const a = 10;
-console.log(a);
+const express = require('express');
+const fs = require('fs');
+
+// App config
+const app = express();
+
+// Middlewares
+const middlewares = [express.json(), express.urlencoded({ extended: true })];
+app.use(middlewares);
+
+app.get('/download', async (req, res, next) => {
+    try {
+        // download file
+
+        res.download('./file.txt');
+    } catch (error) {
+        next(error);
+    }
+});
+
+app.listen(4000, () => {
+    console.log('Server is running on port 4000');
+});
